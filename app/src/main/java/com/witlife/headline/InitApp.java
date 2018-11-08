380e8a10a61832187da3f4f402fa6b51e877d0ac
yitian zhang <yitianchang@gmail.com>
2018-11-09 02:07:09 +1300

    initapp

--- app/src/main/java/com/witlife/headline/InitApp.java
+++ app/src/main/java/com/witlife/headline/InitApp.java
@@ -2,6 +2,11 @@ package com.witlife.headline;
 
 import android.content.Context;
 import android.support.multidex.MultiDexApplication;
+import android.support.v7.app.AppCompatDelegate;
+
+import com.witlife.headline.util.SettingUtil;
+
+import java.util.Calendar;
 
 public class InitApp extends MultiDexApplication {
 
@@ -16,6 +21,25 @@ public class InitApp extends MultiDexApplication {
     }
 
     private void initTheme() {
+        SettingUtil settingUtil = SettingUtil.getInstance();
 
+        if(settingUtil.getIsAutoNightMode()){
+            int nightStartHour = Integer.parseInt(settingUtil.getNightStartHour());
+            int nightStartMinute = Integer.parseInt(settingUtil.getNightStartMinute());
+            int dayStartHour = Integer.parseInt(settingUtil.getDayStartHour());
+            int dayStartMinute = Integer.parseInt(settingUtil.getDayStartMinute());
+
+            Calendar calendar = Calendar.getInstance();
+            int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+            int currentMinute = calendar.get(Calendar.MINUTE);
+
+            int nightValue = nightStartHour * 60 + nightStartHour;
+            int dayValue = dayStartHour * 60 + dayStartMinute;
+            int currentValue = currentHour * 60 + currentMinute;
+
+            if (currentValue >= nightValue || currentValue <= dayValue){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        }
     }
 }
